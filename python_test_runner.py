71746f540fd4906f83ccf82194a33eae8e0e8ff0
Pavel Savchenko <asfaltboy@gmail.com>
2015-07-09 13:52:20 +0200

    misc fixes
    check SublimeANSI plugin exists and add syntax by default
    wrap print statements in DEBUG toggle test

--- python_test_runner.py
+++ python_test_runner.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+import os
 
 import sublime
 import sublime_plugin
 
 DEBUG = False
-DEFAULT_CMD = {'cmd': ["py.test", "-k {selection}"]}
+DEFAULT_CMD = ["py.test", "-k {selection}"]
+
+
+def _log(*args):
+    if not DEBUG:
+        return
+    print(*args)
 
 
 class PythonTestRunnerCommand(sublime_plugin.WindowCommand):
@@ -13,9 +21,16 @@ class PythonTestRunnerCommand(sublime_plugin.WindowCommand):
 
 
 class RunPythonTestsCommand(sublime_plugin.WindowCommand):
+    def ansi_installed(self):
+        return 'sublimeansi' in list(map(str.lower, self.packages))
+
     def setup_runner(self):
         settings = sublime.load_settings("python_test_runner.sublime-settings")
-        print("Settings: %s" % vars(settings))
+        _log("Settings: %s" % vars(settings))
+        self.packages = os.listdir(self.window.extract_variables().get(
+            'packages'))
+
+        _log("Packages: %s" % self.packages)
         # get current filename
         self.filename = self.window.active_view().file_name()
 
@@ -27,16 +42,18 @@ class RunPythonTestsCommand(sublime_plugin.WindowCommand):
             view.settings().set('__vi_external_disable', False)
             if selection:
                 selected_string = view.substr(view.sel()[0])
-                print("Selection: {} ({})".format(selected_string, selection))
+                _log("Selection: {} ({})".format(selected_string, selection))
                 return selected_string
 
     def run(self, *args, **command_kwargs):
-        print("Args: %s" % list(args))
-        print("Kwargs: %s" % command_kwargs)
+        _log("Args: %s" % list(args))
+        _log("Kwargs: %s" % command_kwargs)
         self.setup_runner()
 
-        kwargs = DEFAULT_CMD
-        kwargs['cmd'].append(self.filename)
+        kwargs = {}
+        kwargs['cmd'] = DEFAULT_CMD + [self.filename]
+        if self.ansi_installed:
+            kwargs['syntax'] = "Packages/ANSIescape/ANSI.tmLanguage"
         kwargs.update(command_kwargs)
 
         selection = self.get_selection() or ''
@@ -44,5 +61,6 @@ class RunPythonTestsCommand(sublime_plugin.WindowCommand):
                          for p in kwargs['cmd']]
 
         # return super().run(*args, **kwargs)
-        return self.window.run_command("ansi_color_build", kwargs)
-        # return self.window.run_command("exec", kwargs)
+        if self.ansi_installed:
+            return self.window.run_command("ansi_color_build", kwargs)
+        return self.window.run_command("exec", kwargs)
